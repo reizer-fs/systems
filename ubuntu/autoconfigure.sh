f44e12b3e6326e1b44fa479ddbcfdd355ef8779c
Sebastian Petrovich <sebastianpetrovich@gmail.com>
2019-12-22 19:38:46 +0100

    fix: multiple fix

--- ubuntu/autoconfigure.sh
+++ ubuntu/autoconfigure.sh
@@ -1,48 +1,27 @@
 #!/bin/bash
 
-DATA_DRIVE="/data"
-INSTALL_DIR="/opt/ffx"
-TMUX_DIR="/etc/tmux.conf.d/"
-ALL_DIR="${INSTALL_DIR} ${TMUX_DIR}"
+INSTALL_DIR="$HOME/Git/"
 
-apt-get -q update
-apt-get -q install -y git vim docker.io tmux htop iotop virtualbox
-
-mkdir -p $INSTALL_DIR
-for i in systems scripts windows-client docker ; do 
-	cd $INSTALL_DIR && git clone https://github.com/reizer-fs/$i.git &
-done
-
-# Custom peofile
-rm ~/.bashrc ; ln -s $INSTALL_DIR/scripts/config_host/.bashrc ~/.bashrc
-rm /root/.bashrc ; ln -s $INSTALL_DIR/scripts/config_host/.bashrc /root/.bashrc
-
-for DIR in ${ALL_DIR} ; do
-	[ ! -d "${DIR}" ] &&  mkdir ${DIR} 
-done
-
-# Tmux conf
-[ ! -f /etc/tmux.conf.d/tmux.conf ] && ln -s $INSTALL_DIR/scripts/tmux/tmux.conf /etc/tmux.conf.d/tmux.conf
+sudo apt-get -q update
+sudo apt-get -q install -y openssh-server git vim docker.io tmux htop iotop
 
 # Default Editor 
 update-alternatives --config editor
 
 # Git settings
 git config --global push.default matching
-
 git config --global user.email "sebastianpetrovich@gmail.com"
 git config --global user.name "Sebastian Petrovich"
 
-for DIR in Documents Downloads Pictures Music VM ; do
-	[ ! -L "~/$DIR" ] && [ -d "/data/$DIR" ] && rm -rf ~/$DIR ; ln -s /data/$DIR ~/$DIR
-	ls -d ~/$DIR
-done
-
 # Change Docker data dir to custom path
-systemctl stop docker
-[ ! -L "/var/lib/docker" ] && [ -d "/data/VM/LXC" ] && rm -rf /var/lib/docker ; ln -s /data/VM/LXC/ /var/lib/docker
-systemctl daemon-reload
-systemctl start docker
+#systemctl stop docker
+#[ ! -L "/var/lib/docker" ] && [ -d "/data/VM/LXC" ] && rm -rf /var/lib/docker ; ln -s /data/VM/LXC/ /var/lib/docker
+#systemctl daemon-reload
+#systemctl start docker
+
+# ssh key generation
+ssh-keygen -t rsa -b 4096
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
 # Vmware enable proprietary 3D driver
 #echo 'mks.gl.allowBlacklistedDrivers = "TRUE"' >> ~/.vmware/preferences
@@ -51,10 +30,15 @@ systemctl start docker
 #apt-get install kvm qemu-kvm libvirt-bin virtinst spice-client-gtk gir1.2-spice-client-gtk-3.0
 
 #Add common user to kvm group 
-#usermod -a -G libvirtd fx
-#usermod -a -G kvm fx
+#usermod -a -G libvirtd $(id -un)
+#usermod -a -G kvm $(id -un)
 
 # Python commodities
-apt install python-pip
-pip install sphinx sphinx-autobuild
+sudo apt install python3 python3-pip python3-venv
 
+# Create Ansible python env
+ANSIBLE_VERSIONS="2.9"
+for ansible_version in $ANSIBLE_VERSIONS ; do
+	mkdir -p $HOME/Python/ansible-${ansible_version}
+	python3 -m venv $HOME/Python/ansible-${ansible_version}
+done
