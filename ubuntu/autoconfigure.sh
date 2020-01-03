613e6331b14558eb011c244eacc89c7302b1408f
Sebastian Petrovich <sebastianpetrovich@gmail.com>
2020-01-03 14:20:55 +0100

    fix: clean + additional fix in autoconfigure script

--- ubuntu/autoconfigure.sh
+++ ubuntu/autoconfigure.sh
@@ -3,7 +3,7 @@
 INSTALL_DIR="$HOME/Git/"
 
 sudo apt-get -q update
-sudo apt-get -q install -y openssh-server git vim docker.io tmux htop iotop
+sudo apt-get -q install -y openssh-server git vim tmux htop iotop
 
 # Default Editor 
 update-alternatives --config editor
@@ -13,32 +13,19 @@ git config --global push.default matching
 git config --global user.email "sebastianpetrovich@gmail.com"
 git config --global user.name "Sebastian Petrovich"
 
-# Change Docker data dir to custom path
-#systemctl stop docker
-#[ ! -L "/var/lib/docker" ] && [ -d "/data/VM/LXC" ] && rm -rf /var/lib/docker ; ln -s /data/VM/LXC/ /var/lib/docker
-#systemctl daemon-reload
-#systemctl start docker
-
 # ssh key generation
-ssh-keygen -t rsa -b 4096
+[[ ! -f "~/.ssh/id_rsa" ]] && ssh-keygen -t rsa -b 4096
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
-# Vmware enable proprietary 3D driver
-#echo 'mks.gl.allowBlacklistedDrivers = "TRUE"' >> ~/.vmware/preferences
-
-# Install kvm and libvirt packages
-#apt-get install kvm qemu-kvm libvirt-bin virtinst spice-client-gtk gir1.2-spice-client-gtk-3.0
-
-#Add common user to kvm group 
-#usermod -a -G libvirtd $(id -un)
-#usermod -a -G kvm $(id -un)
-
 # Python commodities
-sudo apt install python3 python3-pip python3-venv
+sudo apt-get -q install -y python3 python3-pip python3-venv
 
 # Create Ansible python env
 ANSIBLE_VERSIONS="2.9"
 for ansible_version in $ANSIBLE_VERSIONS ; do
 	mkdir -p $HOME/Python/ansible-${ansible_version}
 	python3 -m venv $HOME/Python/ansible-${ansible_version}
+	echo "Please run the commands below: 
+	. $HOME/Python/ansible-${ansible_version}
+	pip3 install --upgrade pip ansible"
 done
