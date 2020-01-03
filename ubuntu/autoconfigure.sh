9a0762a783e81ddc35e38e0dd6dc49e76ff4415e
Sebastian Petrovich <sebastianpetrovich@gmail.com>
2020-01-03 22:10:21 +0100

    fix: multiple fix and estethic changes

--- ubuntu/autoconfigure.sh
+++ ubuntu/autoconfigure.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-INSTALL_DIR="$HOME/Git/"
+INSTALL_DIR="$HOME/Git"
 
-sudo apt-get -q update
-sudo apt-get -q install -y openssh-server git vim tmux htop iotop
+sudo apt-get -q update &>/dev/null
+sudo apt-get -q install -y openssh-server git vim tmux htop iotop &>/dev/null
 
 # Default Editor 
-update-alternatives --config editor
+sudo update-alternatives --config editor
 
 # Git settings
 git config --global push.default matching
@@ -14,11 +14,11 @@ git config --global user.email "sebastianpetrovich@gmail.com"
 git config --global user.name "Sebastian Petrovich"
 
 # ssh key generation
-[[ ! -f "~/.ssh/id_rsa" ]] && ssh-keygen -t rsa -b 4096
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+[[ ! -f "$HOME/.ssh/id_rsa" ]] && ssh-keygen -t rsa -b 4096
+grep -qf ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys || cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
 # Python commodities
-sudo apt-get -q install -y python3 python3-pip python3-venv
+sudo apt-get -q install -y python3 python3-pip python3-venv &>/dev/null
 
 # Create Ansible python env
 ANSIBLE_VERSIONS="2.9"
@@ -26,6 +26,11 @@ for ansible_version in $ANSIBLE_VERSIONS ; do
 	mkdir -p $HOME/Python/ansible-${ansible_version}
 	python3 -m venv $HOME/Python/ansible-${ansible_version}
 	echo "Please run the commands below: 
-	. $HOME/Python/ansible-${ansible_version}
+	. $HOME/Python/ansible-${ansible_version}/bin/activate
 	pip3 install --upgrade pip ansible"
+done
+
+for i in systems docker ansible windows-client ; do
+	echo "Processing git repository: $i"
+	[[ ! -d "$INSTALL_DIR/${i}" ]] && /usr/bin/git clone --origin origin git@github.com:reizer-fs/${i}.git $INSTALL_DIR/${i}
 done
